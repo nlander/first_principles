bec634f11d791732d5644df3724db86789d5a99c
Nathan Lander <lander89@gmail.com>
2016-06-22 17:23:40 -0400

    complains about lack of Show instance in one' and oneTwo' even though Char has a show instance.  No longer complains about one' or oneTwo' in main, but complains about testParse stop in main

--- parsersCh24/introExample.hs
+++ parsersCh24/introExample.hs
@@ -9,12 +9,12 @@ stop = unexpected "stop"
 
 one = char '1' <* eof
 
-one' :: Show a => Parser a
+one' :: Parser Char
 one' = one <* stop
 
 oneTwo = char '1' *> char '2'
 
-oneTwo' :: Show a => Parser a
+oneTwo' :: Parser Char
 oneTwo' = char '1' *> char '2' <* stop
 
 testParse :: Show a => Parser a -> IO ()
