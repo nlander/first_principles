448e0b3482aac78e5df50c8c519e6e2a0735314a
Nathan Lander <lander89@gmail.com>
2016-06-22 16:35:42 -0400

    working parser tests

---
+++ parsersCh24/introExample.hs
@@ -0,0 +1,34 @@
+module LearnParsers where
+
+import Text.Trifecta
+import Control.Applicative
+
+stop :: Parser a
+stop = unexpected "stop"
+
+one = char '1' <* eof
+
+one' = one *> stop
+
+oneTwo = char '1' *> char '2'
+
+oneTwo' = char '1' *> char '2' <* stop
+
+testParse :: Parser Char -> IO ()
+testParse p =
+  print $ parseString p mempty "123"
+
+pNL s =
+  putStrLn ('\n' : s)
+
+main = do
+  pNL "stop:"
+  testParse stop
+  pNL "one:"
+  testParse one
+  pNL "one':"
+  testParse one'
+  pNL "oneTwo:"
+  testParse oneTwo
+  pNL "oneTwo':"
+  testParse oneTwo'
