942564a8295054a93a922ce73a275d482c75b761
Nathan Lander <lander89@gmail.com>
2016-06-22 16:47:11 -0400

    complains about oneTwo :: f0 Char, saying f0 has no Applicative or CharParshing instance

--- parsersCh24/introExample.hs
+++ parsersCh24/introExample.hs
@@ -21,14 +21,14 @@ testParse p =
 pNL s =
   putStrLn ('\n' : s)
 
-main = do
-  pNL "stop:"
-  testParse stop
-  pNL "one:"
-  testParse one
-  pNL "one':"
-  testParse one'
-  pNL "oneTwo:"
-  testParse oneTwo
-  pNL "oneTwo':"
-  testParse oneTwo'
+--main = do
+--  pNL "stop:"
+--  testParse stop
+--  pNL "one:"
+--  testParse one
+--  pNL "one':"
+--  testParse one'
+--  pNL "oneTwo:"
+--  testParse oneTwo
+--  pNL "oneTwo':"
+--  testParse oneTwo'
