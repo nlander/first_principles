d9ffc121f310901c00647314b9d06de8e04759f8
Nathan Lander <lander89@gmail.com>
2016-07-22 02:31:08 -0400

    implement Applicative, Functor and Foldable for
    Compose type.

---
+++ composeTypesCh25/applicativeCompose.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE InstanceSigs #-}
+
+newtype Compose f g a =
+  Compose { getCompose :: f (g a) }
+  deriving (Eq, Show)
+
+instance (Functor f, Functor g) =>
+         Functor (Compose f g) where
+  fmap :: (a -> b) -> Compose f g a -> Compose f g b
+  fmap f (Compose a) = Compose $ (fmap . fmap) f a
+
+instance (Applicative f, Applicative g) =>
+         Applicative (Compose f g) where
+  pure :: a -> Compose f g a
+  pure = Compose . pure . pure
+
+  (<*>) :: Compose f g (a -> b)
+        -> Compose f g a
+        -> Compose f g b
+  (Compose f) <*> (Compose g) = Compose $ ((\innerF -> (innerF <*>)) <$> f) <*> g
+
+instance (Foldable f, Foldable g) =>
+         Foldable (Compose f g) where
+  foldMap f (Compose t) = foldMap (foldMap f) t
