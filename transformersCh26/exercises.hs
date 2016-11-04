7200e3bf67ef7413cf49af2aef11f84d499e9c5a
Nathan Lander <lander89@gmail.com>
2016-11-04 02:56:37 -0400

    ReaderT and StateT instances

--- transformersCh26/exercises.hs
+++ transformersCh26/exercises.hs
@@ -35,3 +35,41 @@ eitherT fac fbc (EitherT meab) =
   meab >>= ( \eab -> case eab of
                (Left a)  -> fac a
                (Right b) -> fbc b )
+
+newtype ReaderT r m a =
+  ReaderT { runReaderT :: r -> m a }
+
+instance Functor m => Functor (ReaderT r m) where
+  fmap fab (ReaderT frma) = ReaderT ( \r -> fab <$> frma r )
+
+instance Applicative m => Applicative (ReaderT r m) where
+  pure = ReaderT . const . pure
+  (ReaderT frmab) <*> (ReaderT frma) = ReaderT ( \r -> frmab r <*> frma r )
+
+instance Monad m => Monad (ReaderT r m) where
+  return = pure
+  (ReaderT frma) >>= farmb = ReaderT ( \r -> frma r >>= ( \a -> ( (runReaderT . farmb) a ) r ) )
+
+newtype StateT s m a =
+  StateT { runStateT :: s -> m (a, s) }
+
+instance Functor m => Functor (StateT s m) where
+  fmap fab (StateT fsmas) = StateT $
+    \s -> fmap
+            (\tup -> (fab (fst tup), snd tup))
+            ( fsmas s )
+
+instance Monad m => Applicative (StateT s m) where
+  pure a = StateT $ \s -> pure (a,s)
+  (StateT fsmabs) <*> (StateT fsmas) =
+    StateT $ \s -> 
+      fsmas s >>= 
+      ( \(a,s0) ->
+          fmap
+            ( \(f, s1) -> (f a, s1) )
+            ( fsmabs s0 ) )
+
+instance Monad m => Monad (StateT s m) where
+  return = pure
+  (StateT fsmas) >>= fasmbs = 
+    StateT $ ( \s -> fsmas s >>= ( \(a,s0) -> (runStateT . fasmbs) a s0 ) )
